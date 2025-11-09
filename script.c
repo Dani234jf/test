4ceb425a4de4c7d703b3da4704002366dc2dcfb0
ImPatrickRg26 <patricio.reinoso.guerrero@ubi.pt>
2025-11-09 13:10:38 +0000

    Ponto adicionado no printf

--- script.c
+++ script.c
@@ -6,7 +6,7 @@ int add(int a , int b) {
 
 int main() {
     int x, y ;
-    printf("soma = %d ",add(x,y));
+    printf("soma: %d ",add(x,y));
     printf("Hello, world. Its new test!");
     return 0;
 }
